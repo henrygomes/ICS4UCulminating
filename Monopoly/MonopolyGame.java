e32c09e54f0fe5795f68bcc4d74f4af625997bd8
henrygomes <hajgomes@outlook.com>
2018-04-28 15:30:36 -0400

    Added methods
    added methods to monopolyGame and changed GUI class to Print class

--- Monopoly/MonopolyGame.java
+++ Monopoly/MonopolyGame.java
@@ -7,5 +7,37 @@
  */
 public class MonopolyGame implements Game
 {
+    public MonopolyGame()
+    {
+        //how many players?
+        //initializing players
+        //starting game
+    }
     
+    public static void turn (int numOfPlayers)
+    {
+        //loop for turn
+    }
+    
+    public static int roll()
+    {
+        //using rand, possibly two rands to generate similar # to real game
+        return 0; 
+    }
+    
+    public static void land (int space)
+    {
+        
+        //when player lands: do you want to buy property?, do you have to pay someone?       
+    }
+    
+    public static void boardCreation()
+    {
+        //creating the board, properies, etc.
+    }    
+    
+    public static void print (Space[] board)
+    {
+        
+    }
 }
---
+++ Monopoly/Print.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class GUI here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Print
+{
+   public Print (Space[] board)
+   {
+       
+   }
+}
