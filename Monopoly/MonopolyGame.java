4c721a571fa189331b223c21ca883e0eb9a6f422
henrygomes <hajgomes@outlook.com>
2018-04-28 13:24:39 -0400

    turned the zip file into a folder

---
+++ Monopoly/MonopolyGame.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class MonopolyGame here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MonopolyGame implements Game
+{
+    
+}
---
+++ Monopoly/Property.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Property here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Property extends Space
+{
+   
+}
