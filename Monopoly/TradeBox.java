c049e6a91cf6d872ddbdaeb620147640f27ff100
324570159 <ian@sepal.ca>
2018-05-23 23:45:52 -0500

    adding trade box

--- Monopoly/TradeBox.java
+++ Monopoly/TradeBox.java
@@ -1,11 +1,14 @@
  
 
+import java.util.*;
+//import javafx.beans.property.Property;
 import javafx.scene.control.Button;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
+import java.util.ArrayList;
 
 
 public class TradeBox
@@ -13,41 +16,66 @@ public class TradeBox
 
     static String answer;
 
-    public static String display(String title, String[] info1, String[] info2, String message1, String message2, String tip)
+    public static ArrayList<Property>[] display(String title, ArrayList<Property> info1, ArrayList<Property> info2, String message1, String message2, String tip)
     {
+        System.out.println("in Trade");
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMinWidth(250);
-        Label label = new Label();
+        ArrayList<Property> trade1 = new ArrayList<Property>();
+        ArrayList<Property> trade2 = new ArrayList<Property>();
+        ArrayList<Property>[] tradeReturn = new ArrayList[2];
+
         ComboBox<String> player1 = new ComboBox<>();
-        player1.setTooltip(new Tooltip(tip));
         player1.setPromptText(message1);
-
+        player1.setTooltip(new Tooltip(tip));
+        
         ComboBox<String> player2 = new ComboBox<>();
-        player2.setTooltip(new Tooltip(tip));
         player2.setPromptText(message2);
+        player2.setTooltip(new Tooltip(tip));
+
+        int info1size = info1.size();
+        int info2size = info2.size();
 
 
+        for(int x = 0; x < info1size; x++)
+            player1.getItems().add(info1.get(x).getName());
 
-        for(int x = 0; x < info1.length; x++)
-            player1.getItems().add(info1[x]);
-
-        for(int x = 0; x < info2.length; x++)
-            player2.getItems().add(info2[x]);
-
+        for(int x = 0; x < info2size; x++)
+            player2.getItems().add(info2.get(x).getName());
 
 
-        Button sendButton = new Button("Send");
-        Button acceptButton = new Button("Accept Trade");
+        Button tradeButtonPlayer1 = new Button("Add To Trade");
+        Button tradeButtonPlayer2 = new Button("Add To Trade");
+        Button tradeButton = new Button("Finalize Trade");
         Button closeButton = new Button("Close Trade");
 
-        sendButton.setOnAction(e -> {
-            answer = player1.getValue();
-            window.close();
+        tradeButtonPlayer1.setOnAction(e -> {
+            for (int x = 0; x < info1size; x++) {
+                if (player1.getValue() == info1.get(x).getName())
+                    trade1.add(info1.get(x));
+            }
+
         });
 
-        acceptButton.setOnAction(e -> {
+        tradeButtonPlayer2.setOnAction(e -> {
+            for (int x = 0; x < info2size; x++) {
+                if (player2.getValue() == info2.get(x).getName())
+                    trade2.add(info2.get(x));
+            }
+        });
+
+        /*
+        tradeButtonPlayer2.setOnAction(e -> {
+            trade2.add(player2.getValue());
+        });
+        */
+
+        tradeButton.setOnAction(e -> {
+            tradeReturn[0] = trade1;
+            tradeReturn[1] = trade2;
+            System.out.println("returning");
             window.close();
         });
 
@@ -55,7 +83,6 @@ public class TradeBox
             window.close();
         });
 
-        //comboBox.setOnAction(e -> System.out.println("User Selected: " + comboBox.getValue()));
 
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10, 30, 10, 30));
@@ -67,17 +94,20 @@ public class TradeBox
         passInput.setPromptText(tip);
 
 
-        //GridPane.setConstraints(comboBox, 0, 0);
-        //GridPane.setConstraints(selectButton, 1, 0);
-        //GridPane.setConstraints(backButton, 2, 0);
+        GridPane.setConstraints(player1, 0, 0);
+        GridPane.setConstraints(tradeButtonPlayer1, 1, 0);
+        GridPane.setConstraints(player2, 2, 0);
+        GridPane.setConstraints(tradeButtonPlayer2, 3, 0);
+        GridPane.setConstraints(tradeButton, 1, 2);
+        GridPane.setConstraints(closeButton, 2, 2);
 
-        //grid.getChildren().addAll(comboBox, selectButton, backButton);
+        grid.getChildren().addAll(player1, tradeButtonPlayer1, player2, tradeButtonPlayer2, tradeButton, closeButton);
 
 
-        Scene scene = new Scene(grid, 400, 50);
+        Scene scene = new Scene(grid, 700, 100);
         window.setScene(scene);
         window.showAndWait();
-        return answer;
+        return tradeReturn;
 
     }
 }
