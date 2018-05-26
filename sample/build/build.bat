0a591c58af8644633a57e034b79f48160906d39a
324570159 <ian@sepal.ca>
2018-05-26 10:24:21 -0500

    added relative paths
    Use build.bat to compile and monopoly.bat to run. Need to point JDK path to the install of JDK - first line in each bat file

--- sample/build/build.bat
+++ sample/build/build.bat
@@ -1,5 +1,4 @@
 SET JDK=C:\Users\Ian\.IdeaIC2018.1\config\jdks\jbsdk8u112b287.2_windows_x64
-SET CODE_BUILD_DIR=C:\Users\Ian\Documents\GitHub\Monopoly\sample\out
 
 %JDK%\bin\javac -Xlint:unchecked -d . ..\*.java
 COPY ..\sample.fxml .\sample
--- sample/monopoly.bat
+++ sample/monopoly.bat
@@ -1,6 +1,9 @@
+REM Set to path of JDK install. Can download from Oracle
 SET JDK=C:\Users\Ian\.IdeaIC2018.1\config\jdks\jbsdk8u112b287.2_windows_x64
+
+
 SET CLASSPATH=%JDK%\jre\lib\charsets.jar;%JDK%\jre\lib\ext\access-bridge-64.jar;%JDK%\jre\lib\ext\cldrdata.jar;%JDK%\jre\lib\ext\dnsns.jar;%JDK%\jre\lib\ext\jaccess.jar;%JDK%\jre\lib\ext\jfxrt.jar;%JDK%\jre\lib\ext\localedata.jar;%JDK%\jre\lib\ext\nashorn.jar;%JDK%\jre\lib\ext\sunec.jar;%JDK%\jre\lib\ext\sunjce_provider.jar;%JDK%\jre\lib\ext\sunmscapi.jar;%JDK%\jre\lib\ext\sunpkcs11.jar;%JDK%\jre\lib\ext\zipfs.jar;%JDK%\jre\lib\jce.jar;%JDK%\jre\lib\jfxswt.jar;%JDK%\jre\lib\jsse.jar;%JDK%\jre\lib\management-agent.jar;%JDK%\jre\lib\resources.jar;%JDK%\jre\lib\rt.jar
-SET CODE_BUILD_DIR=C:\Users\Ian\Documents\GitHub\Monopoly\sample\build
+SET CODE_BUILD_DIR=.\build
 
 %JDK\bin\javac ..\Main.class
 %JDK%\bin\java.exe -Dfile.encoding=UTF-8 -classpath %CLASSPATH%;%CODE_BUILD_DIR% sample.Main
